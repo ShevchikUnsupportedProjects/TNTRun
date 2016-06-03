0d25697ea75d8af7e899e728de793de241d8ab98
tade159SK <tadeas123741@gmail.com>
2016-06-03 22:41:55 +0200

    Forgot to remove debug

--- src/tntrun/arena/structure/GameZone.java
+++ src/tntrun/arena/structure/GameZone.java
@@ -1,20 +1,3 @@
-/**
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- */
-
 package tntrun.arena.structure;
 
 import java.util.ConcurrentModificationException;
@@ -181,7 +164,6 @@ public class GameZone {
             			bs.update(true);
             			bsit.remove();
             		}else{
-            			TNTRun.getInstance().getLogger().info("Stoped");
             			cancel();
             		}
                 	/*if(bsi.hasNext()) {
