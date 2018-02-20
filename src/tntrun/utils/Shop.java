330facc15ed85d8afe211f91d478472bc14feccc
BuildTools <steve4744@gmail.com>
2018-02-20 02:01:08 +0000

    refactored shop to not use itemIds, fixed enchants

--- src/tntrun/utils/Shop.java
+++ src/tntrun/utils/Shop.java
@@ -7,7 +7,7 @@ import java.util.List;
 import net.milkbowl.vault.economy.Economy;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Sound;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -24,11 +24,12 @@ import tntrun.messages.Messages;
 
 public class Shop implements Listener{
 	
-	private TNTRun pl;
+	private static TNTRun pl;
 	
 	public Shop(TNTRun plugin){
 		pl = plugin;
-		ShopFiles.setShopItems();
+		ShopFiles shopFiles = new ShopFiles(pl);
+		shopFiles.setShopItems();
 		
 		invsize = pl.getConfig().getInt("shop.size");
 		invname = pl.getConfig().getString("shop.name").replace("&", "§");
@@ -41,13 +42,13 @@ public class Shop implements Listener{
 	public static int invsize; 
 	
 	private void giveItem(int slot, Player player, String title) {
-		int kit = itemSlot.get(slot);
+		int kit = itemSlot.get(slot);		
 		ArrayList<ItemStack> item = new ArrayList<ItemStack>();
 		FileConfiguration cfg = ShopFiles.getShopConfiguration();
+		
 		for(String items : cfg.getConfigurationSection(kit + ".items").getKeys(false)) {
-			try {
-				int ID = Integer.valueOf(cfg.getInt(kit + ".items." + items + ".ID"));
-				int subID = Integer.valueOf(cfg.getInt(kit + ".items." + items + ".subID"));
+			try {				
+				Material material = Material.getMaterial(cfg.getString(kit + ".items." + items + ".material"));
 				int amount = Integer.valueOf(cfg.getInt(kit + ".items." + items + ".amount"));
 				String displayname = cfg.getString(kit + ".items." + items + ".displayname").replace("&", "§");
 				List<String> lore = cfg.getStringList(kit + ".items." + items + ".lore");
@@ -57,8 +58,7 @@ public class Shop implements Listener{
 					bought.add(player);
 				}
 				
-				item.add(getItem(ID, subID, amount, displayname, lore, enchantments));
-				
+				item.add(getItem(material, amount, displayname, lore, enchantments));				
 				player.updateInventory();
 				player.closeInventory();
 			} catch(Exception e) {
@@ -68,131 +68,137 @@ public class Shop implements Listener{
 		pitems.put(player, item);
 	}
 
-	  private ItemStack getItem(int ID, int subID, int amount, String displayname, List<String> lore, List<String> enchantments){
-	    ItemStack item = new ItemStack(ID, amount, (short)subID);
+	private ItemStack getItem(Material material, int amount, String displayname, List<String> lore, List<String> enchantments){
+		
+		ItemStack item = new ItemStack(material, amount);
 	    ItemMeta meta = item.getItemMeta();
 	    meta.setDisplayName(displayname);
+	    
 	    if ((lore != null) && (!lore.isEmpty())) {
 	      meta.setLore(lore);
 	    }
+	    
 	    if ((enchantments != null) && (!enchantments.isEmpty())) {
 	      for (String enchs : enchantments) {
-	        String[] array = enchs.split(":");
-	        String ench = array[0];
+	        String[] array = enchs.split("#");
+	        String ench = array[0].toUpperCase();
 	        int level = Integer.valueOf(array[1]).intValue();
 	        Enchantment realEnch = Enchantment.getByName(ench);
-	        meta.addEnchant(realEnch, level, true);
+	        if (realEnch != null) {
+	        	meta.addEnchant(realEnch, level, true);
+	        }
 	      }
 	    }
 	    item.setItemMeta(meta);
 	    return item;
-	  }
-	  @EventHandler
-	  public void onClick(InventoryClickEvent e){
+	}
+	@EventHandler
+	public void onClick(InventoryClickEvent e){
 	    Player p = (Player)e.getWhoClicked();
-	    if (e.getInventory().getName().equals(invname)) {
-	      e.setCancelled(true);
-	      if ((e.getSlot() == e.getRawSlot()) && 
-	        (e.getCurrentItem() != null)) {
-	        ItemStack current = e.getCurrentItem();
-	        if ((current.hasItemMeta()) && (current.getItemMeta().hasDisplayName())) {
-	          int kit = ((Integer)itemSlot.get(Integer.valueOf(e.getSlot()))).intValue();
-	          FileConfiguration cfg = ShopFiles.getShopConfiguration();
-	          String permission = cfg.getString(kit + ".permission");
-	          if(bought.contains(p)){
-	        	  p.sendMessage(Messages.alreadyboughtitem.replace("&", "§"));
-	        	  TNTRun.getInstance().sound.WITHER_HURT(p, 5, 999);
-	        	  return;
-	          }
-	          if (p.hasPermission(permission)) {
-	        	  String title = current.getItemMeta().getDisplayName();
-	        	  int cost = cfg.getInt(kit + ".cost");
+	    
+	    if (e.getInventory().getName().equals(invname)) { 	
+	    	e.setCancelled(true);
+	    	if ((e.getSlot() == e.getRawSlot()) && (e.getCurrentItem() != null)) {
+	    		ItemStack current = e.getCurrentItem();
+	    		if ((current.hasItemMeta()) && (current.getItemMeta().hasDisplayName())) {
+	    			int kit = ((Integer)itemSlot.get(Integer.valueOf(e.getSlot()))).intValue();
+	    			FileConfiguration cfg = ShopFiles.getShopConfiguration();
+	    			String permission = cfg.getString(kit + ".permission");
+	    			
+	    			if(bought.contains(p)){
+	    				p.sendMessage(Messages.alreadyboughtitem.replace("&", "§"));
+	    				TNTRun.getInstance().sound.WITHER_HURT(p, 5, 999);
+	    				return;
+	    			}
+	    			if (p.hasPermission(permission)) {
+	    				String title = current.getItemMeta().getDisplayName();
+	    				int cost = cfg.getInt(kit + ".cost");
 	        	  
-	        	  if(cfg.getInt(kit + ".ID") == 288){
-						if((pl.getConfig().getInt("shop.doublejump.maxdoublejumps") <= pl.getConfig().getInt("doublejumps." + p.getName()))){
-				        	  p.sendMessage(Messages.alreadyboughtitem.replace("&", "§"));
-				        	  TNTRun.getInstance().sound.WITHER_HURT(p, 5, 999);
-				        	  return;
-						}
-	        	  }
+	    				if (Material.getMaterial(cfg.getString(kit + ".material").toUpperCase()) == Material.FEATHER) {
+	    					if((pl.getConfig().getInt("shop.doublejump.maxdoublejumps") <= pl.getConfig().getInt("doublejumps." + p.getName()))){
+	    						p.sendMessage(Messages.alreadyboughtitem.replace("&", "§"));
+	    						TNTRun.getInstance().sound.WITHER_HURT(p, 5, 999);
+	    						return;
+	    					}
+	    				}
 	        	  
-	        	  if(hasMoney(cost, p)) {
-	        		  p.sendMessage(Messages.playerboughtitem.replace("&", "§").replace("{ITEM}", title).replace("{MONEY}", cost + ""));
-	        		  p.sendMessage(Messages.playerboughtwait.replace("&", "§"));
-	  				  TNTRun.getInstance().sound.NOTE_PLING(p, 5, 10);
-	        	  }else{
-	        		  p.sendMessage(Messages.notenoughtmoney.replace("&", "§").replace("{MONEY}", cost + ""));
-	        		  TNTRun.getInstance().sound.WITHER_HURT(p, 5, 999);
-	        		  return;
-	        	  }
-		          if(cfg.getInt(kit + ".ID") == 288){
-						if(pl.getConfig().get("doublejumps." + p.getName()) == null){
-							pl.getConfig().set("doublejumps." + p.getName(), 1);
-						}else{
-							pl.getConfig().set("doublejumps." + p.getName(), pl.getConfig().getInt("doublejumps." + p.getName()) + 1);
-						}
-						pl.saveConfig();
-						return;
-		          }
-	            giveItem(e.getSlot(), p, current.getItemMeta().getDisplayName());  
-	          } else {
-	            p.closeInventory();
-	            p.sendMessage(Messages.nopermission.replace("&", "§"));
-	            TNTRun.getInstance().sound.WITHER_HURT(p, 5, 999);
-	          }
-	        }
-	      }
+	    				if(hasMoney(cost, p)) {
+	    					p.sendMessage(Messages.playerboughtitem.replace("&", "§").replace("{ITEM}", title).replace("{MONEY}", cost + ""));
+	    					p.sendMessage(Messages.playerboughtwait.replace("&", "§"));
+	    					TNTRun.getInstance().sound.NOTE_PLING(p, 5, 10);
+	    				}else{
+	    					p.sendMessage(Messages.notenoughtmoney.replace("&", "§").replace("{MONEY}", cost + ""));
+	    					TNTRun.getInstance().sound.WITHER_HURT(p, 5, 999);
+	    					return;
+	    				}
+	    				if (Material.getMaterial(cfg.getString(kit + ".material").toUpperCase()) == Material.FEATHER) {
+	    					if(pl.getConfig().get("doublejumps." + p.getName()) == null){
+	    						pl.getConfig().set("doublejumps." + p.getName(), 1);
+	    					}else{
+	    						pl.getConfig().set("doublejumps." + p.getName(), pl.getConfig().getInt("doublejumps." + p.getName()) + 1);
+	    					}
+	    					pl.saveConfig();
+	    					return;
+	    				}
+	    				giveItem(e.getSlot(), p, current.getItemMeta().getDisplayName());  
+	    			} else {
+	    				p.closeInventory();
+	    				p.sendMessage(Messages.nopermission.replace("&", "§"));
+	    				TNTRun.getInstance().sound.WITHER_HURT(p, 5, 999);
+	    			}
+	    		}
+	    	}
 	    }
-	  }
+	}
 	  
-		private Object economy = null;
+	private Object economy = null;
 		
-		private boolean hasMoney(int moneyneed, Player player) {
-			if (Bukkit.getPluginManager().getPlugin("Vault") != null) {
-				RegisteredServiceProvider<Economy> economyProvider = Bukkit.getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
-				if (economyProvider != null) {
-					economy = economyProvider.getProvider();
-				}
+	private boolean hasMoney(int moneyneed, Player player) {
+		if (Bukkit.getPluginManager().getPlugin("Vault") != null) {
+			RegisteredServiceProvider<Economy> economyProvider = Bukkit.getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+			if (economyProvider != null) {
+				economy = economyProvider.getProvider();
 			}
-			 
-			if (economy != null) {
-				Economy econ = (Economy) economy;
-				double pmoney = econ.getBalance(player.getName());
-				if(pmoney >= moneyneed){
-					econ.withdrawPlayer(player.getName(), moneyneed);
-					return true;
-				}
-			}
-			return false;
 		}
+			 
+		if (economy != null) {
+			Economy econ = (Economy) economy;
+			double pmoney = econ.getBalance(player.getName());
+			if(pmoney >= moneyneed){
+				econ.withdrawPlayer(player.getName(), moneyneed);
+				return true;
+			}
+		}
+		return false;
+	}
 		
-		  public static void setItems(Inventory inventory){
-		    FileConfiguration cfg = ShopFiles.getShopConfiguration();
-		    int slot = 0;
-		    for (String kitCounter : cfg.getConfigurationSection("").getKeys(false)) {
+	public static void setItems(Inventory inventory){
+		FileConfiguration cfg = ShopFiles.getShopConfiguration();
+		int slot = 0;
+		for (String kitCounter : cfg.getConfigurationSection("").getKeys(false)) {
 		      String title = cfg.getString(kitCounter + ".name").replace("&", "§");
 		      List<String> lore = new ArrayList<String>();
 		      for (String loreLines : cfg.getStringList(kitCounter + ".lore")) {
 		    	  loreLines = loreLines.replace("&", "§");
-		        lore.add(loreLines);
+		          lore.add(loreLines);
 		      }
-		      int ID = cfg.getInt(kitCounter + ".ID");
-		      int subID = cfg.getInt(kitCounter + ".subID");
+		      Material material = Material.getMaterial(cfg.getString(kitCounter + ".material"));
 		      int amount = cfg.getInt(kitCounter + ".amount");
-		      inventory.setItem(slot, getItem(ID, subID, title, lore, amount));
+		      inventory.setItem(slot, getItem(material, title, lore, amount));
 		      itemSlot.put(Integer.valueOf(slot), Integer.valueOf(kitCounter));
 		      slot++;
-		    }
-		  }
+		}
+	}
 
-		  private static ItemStack getItem(int ID, int subID, String title, List<String> lore, int amount){
-		    ItemStack item = new ItemStack(ID, amount, (short)subID);
-		    ItemMeta meta = item.getItemMeta();
-		    meta.setDisplayName(title);
-		    if ((lore != null) && (!lore.isEmpty())) {
-		      meta.setLore(lore);
-		    }
-		    item.setItemMeta(meta);
-		    return item;
-		  }
+	private static ItemStack getItem(Material material, String title, List<String> lore, int amount){
+		ItemStack item = new ItemStack(material, amount);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(title);
+		
+		if ((lore != null) && (!lore.isEmpty())) {
+		    meta.setLore(lore);
+		}
+		item.setItemMeta(meta);
+		return item;
+	}
 }
--- src/tntrun/utils/ShopFiles.java
+++ src/tntrun/utils/ShopFiles.java
@@ -8,39 +8,48 @@ import java.util.List;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
+import tntrun.TNTRun;
+
 public class ShopFiles {
+	private static TNTRun pl;
+	public ShopFiles(TNTRun plugin) {
+		pl = plugin;
+	}
 
 	public static File getShopFile(){
-	   return new File("plugins/TNTRun", "shop.yml");
+	   return new File(pl.getDataFolder(), "shop.yml");
 	}
 
 	public static FileConfiguration getShopConfiguration() {
 	   return YamlConfiguration.loadConfiguration(getShopFile());
 	}
 
-	public static void setShopItems() {
+	public void setShopItems() {
 	   FileConfiguration cfg = getShopConfiguration();
 	   cfg.options().copyDefaults(true);
 	   cfg.addDefault("1.name", "&7Default double jump");
 	   cfg.addDefault("1.cost", Integer.valueOf(100));
-	   cfg.addDefault("1.ID", Integer.valueOf(288));
-	   cfg.addDefault("1.subID", Integer.valueOf(0));
+	   cfg.addDefault("1.material", "FEATHER");
 	   cfg.addDefault("1.amount", Integer.valueOf(1));
 	   cfg.addDefault("1.permission", "shop.default");
 	   List<String> lore = new ArrayList<String>();
 	   lore.add("Basic kit");
 	   lore.add("Cost 100 coins");
 	   cfg.addDefault("1.lore", lore);
-	   cfg.addDefault("1.items.1.ID", Integer.valueOf(0));
-	   cfg.addDefault("1.items.1.subID", Integer.valueOf(0));
+	   cfg.addDefault("1.items.1.material", "");
 	   cfg.addDefault("1.items.1.amount", Integer.valueOf(1));
 	   cfg.addDefault("1.items.1.displayname", "&cDefault");
 	   List<String> iLore = new ArrayList<String>();
 	   iLore.add("Basic kit - double jump");
 	   cfg.addDefault("1.items.1.lore", iLore);
 	   List<String> iEnch = new ArrayList<String>();
-	   iEnch.add("KNOCKBACK:1");
+	   iEnch.add("KNOCKBACK#1");
 	   cfg.addDefault("1.items.1.enchantments", iEnch);
+	   //tidy up old IDs
+	   cfg.set("1.ID", null);
+	   cfg.set("1.subID", null);
+	   cfg.set("1.items.1.ID", null);
+	   cfg.set("1.items.1.subID", null);
 	   try{
 	     cfg.save(getShopFile());
 	   } catch (IOException e) {
