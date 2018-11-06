9bacf12f1868571f9ffce588c2d3f56c8776a57b
BuildTools <steve4744@gmail.com>
2018-11-06 18:16:08 +0000

    repackage add/delete kits as they are not linked to a single arena

--- src/tntrun/commands/setup/kits/AddKit.java
+++ src/tntrun/commands/setup/kits/AddKit.java
@@ -15,7 +15,7 @@
  *
  */
 
-package tntrun.commands.setup.arena;
+package tntrun.commands.setup.kits;
 
 import org.bukkit.entity.Player;
 
@@ -23,10 +23,10 @@ import tntrun.TNTRun;
 import tntrun.arena.Arena;
 import tntrun.commands.setup.CommandHandlerInterface;
 
-public class DeleteKit implements CommandHandlerInterface {
+public class AddKit implements CommandHandlerInterface {
 
 	private TNTRun plugin;
-	public DeleteKit(TNTRun plugin) {
+	public AddKit(TNTRun plugin) {
 		this.plugin = plugin;
 	}
 
@@ -41,8 +41,8 @@ public class DeleteKit implements CommandHandlerInterface {
 			player.sendMessage("Disable arena first");
 			return true;
 		}
-		arena.getStructureManager().getKits().unregisterKit(args[1]);
-		player.sendMessage("Kit deleted");
+		arena.getStructureManager().getKits().registerKit(args[1], player);
+		player.sendMessage("Kit added");
 		return true;
 	}
 
--- src/tntrun/commands/setup/kits/DeleteKit.java
+++ src/tntrun/commands/setup/kits/DeleteKit.java
@@ -15,7 +15,7 @@
  *
  */
 
-package tntrun.commands.setup.arena;
+package tntrun.commands.setup.kits;
 
 import org.bukkit.entity.Player;
 
@@ -23,10 +23,10 @@ import tntrun.TNTRun;
 import tntrun.arena.Arena;
 import tntrun.commands.setup.CommandHandlerInterface;
 
-public class AddKit implements CommandHandlerInterface {
+public class DeleteKit implements CommandHandlerInterface {
 
 	private TNTRun plugin;
-	public AddKit(TNTRun plugin) {
+	public DeleteKit(TNTRun plugin) {
 		this.plugin = plugin;
 	}
 
@@ -41,8 +41,8 @@ public class AddKit implements CommandHandlerInterface {
 			player.sendMessage("Disable arena first");
 			return true;
 		}
-		arena.getStructureManager().getKits().registerKit(args[1], player);
-		player.sendMessage("Kit added");
+		arena.getStructureManager().getKits().unregisterKit(args[1]);
+		player.sendMessage("Kit deleted");
 		return true;
 	}
 
