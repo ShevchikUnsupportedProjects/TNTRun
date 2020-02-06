cab8e8b7947a6e873d14dd2cffcbac3de9fa0474
BuildTools <steve4744@gmail.com>
2020-02-06 23:55:48 +0000

    fix whitespace

--- src/tntrun/MySQL.java
+++ src/tntrun/MySQL.java
@@ -18,6 +18,7 @@
 package tntrun;
 
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -27,207 +28,203 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Logger;
 
-public class MySQL {
-    private static final Logger logger = Bukkit.getLogger();
-    protected boolean connected = false;
-    public Connection c = null;
-    private String driver;
-    private String connectionString;
-    private TNTRun pl;
+	public class MySQL {
+	private static final Logger logger = Bukkit.getLogger();
+	protected boolean connected = false;
+	public Connection c = null;
+	private String driver;
+	private String connectionString;
+	private TNTRun pl;
 
-    protected static enum Statements {
-        SELECT, INSERT, UPDATE, DELETE, DO, REPLACE, LOAD, HANDLER, CALL, CREATE, ALTER, DROP, TRUNCATE, RENAME, START,
-        COMMIT, ROLLBACK, SAVEPOINT, LOCK, UNLOCK, PREPARE, EXECUTE, DEALLOCATE, SET, SHOW, DESCRIBE, EXPLAIN, HELP,
-        USE, ANALYZE, ATTACH, BEGIN, DETACH, END, INDEXED, ON, PRAGMA, REINDEX, RELEASE, VACUUM;
-    }
+	protected static enum Statements {
+		SELECT, INSERT, UPDATE, DELETE, DO, REPLACE, LOAD, HANDLER, CALL, CREATE, ALTER, DROP, TRUNCATE, RENAME, START,
+		COMMIT, ROLLBACK, SAVEPOINT, LOCK, UNLOCK, PREPARE, EXECUTE, DEALLOCATE, SET, SHOW, DESCRIBE, EXPLAIN, HELP,
+		USE, ANALYZE, ATTACH, BEGIN, DETACH, END, INDEXED, ON, PRAGMA, REINDEX, RELEASE, VACUUM;
+	}
 
-    public MySQL(TNTRun plugin) {
-        this.pl = plugin;
-    }
+	public MySQL(TNTRun plugin) {
+		this.pl = plugin;
+	}
 
-    public MySQL(String hostname, int port, String database, String username, String password,
-                           TNTRun plugin) {
-        driver           = "com.mysql.jdbc.Driver";
-        connectionString = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?user=" + username
-                           + "&password=" + password;
-        this.pl = plugin;
-    }
+	public MySQL(String hostname, int port, String database, String username, String password, TNTRun plugin) {
+		driver           = "com.mysql.jdbc.Driver";
+		connectionString = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?user=" + username + "&password=" + password;
+		this.pl = plugin;
+	}
 
-    public Connection open() {
-        try {
-            Class.forName(driver);
-            this.c = DriverManager.getConnection(connectionString);
+	public Connection open() {
+		try {
+			Class.forName(driver);
+			this.c = DriverManager.getConnection(connectionString);
+			return c;
+		} catch (SQLException e) {
+			System.out.println("Could not connect to Database! because: " + e.getMessage());
+		} catch (ClassNotFoundException e) {
+			System.out.println(driver + " not found!");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 
-            return c;
-        } catch (SQLException e) {
-            System.out.println("Could not connect to Database! because: " + e.getMessage());
-        } catch (ClassNotFoundException e) {
-            System.out.println(driver + " not found!");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+		return this.c;
+	}
 
-        return this.c;
-    }
+	public Connection getConn() {
+		return this.c;
+	}
 
-    public Connection getConn() {
-        return this.c;
-    }
+	public void close() {
+		try {
+			if (c != null) {
+				c.close();
+			}
+		} catch (SQLException ex) {
+			pl.getLogger().info(ex.getMessage());
+		}
 
-    public void close() {
-        try {
-            if (c != null) {
-                c.close();
-            }
-        } catch (SQLException ex) {
-            pl.getLogger().info(ex.getMessage());
-        }
+		c = null;
+	}
 
-        c = null;
-    }
+	public boolean isConnected() {
+		try {
+			return (((c == null) || c.isClosed())
+					? false
+					: true);
+		} catch (SQLException e) {
+			return false;
+		}
+	}
 
-    public boolean isConnected() {
-        try {
-            return (((c == null) || c.isClosed())
-                    ? false
-                    : true);
-        } catch (SQLException e) {
-            return false;
-        }
-    }
+	public Result query(final String query) {
+		if (!isConnected()) {
+			open();
+		}
 
-    public Result query(final String query) {
-        if (!isConnected()) {
-            open();
-        }
+		return query(query, true);
+	}
 
-        return query(query, true);
-    }
+	public Result query(final String query, boolean retry) {
+		if (!isConnected()) {
+			open();
+		}
+		try {
+			PreparedStatement statement = null;
 
-    public Result query(final String query, boolean retry) {
-        if (!isConnected()) {
-            open();
-        }
+			try {
+				if (!isConnected()) {
+					open();
+				}
 
-        try {
-            PreparedStatement statement = null;
+				statement = c.prepareStatement(query);
 
-            try {
-                if (!isConnected()) {
-                    open();
-                }
+				if (statement.execute()) {
+					return new Result(statement, statement.getResultSet());
+				}
+			} catch (final SQLException e) {
+				final String msg = e.getMessage();
 
-                statement = c.prepareStatement(query);
+				logger.severe("Database query error: " + msg);
 
-                if (statement.execute()) {
-                    return new Result(statement, statement.getResultSet());
-                }
-            } catch (final SQLException e) {
-                final String msg = e.getMessage();
+				if (retry && msg.contains("_BUSY")) {
+					logger.severe("Retrying query...");
+					new BukkitRunnable() {
+						@Override
+						public void run() {
+							query(query, false);
+						}
+					}.runTaskLater(pl, 20L);
+				}
+			}
 
-                logger.severe("Database query error: " + msg);
+			if (statement != null) {
+				statement.close();
+			}
+		} catch (SQLException ex) {
+			pl.getLogger().info(ex.getMessage());
+		}
 
-                if (retry && msg.contains("_BUSY")) {
-                    logger.severe("Retrying query...");
-                    pl.getServer().getScheduler().scheduleSyncDelayedTask(pl, new Runnable() {
-                        @Override
-                        public void run() {
-                            query(query, false);
-                        }
-                    }, 20);
-                }
-            }
+		return null;
+	}
 
-            if (statement != null) {
-                statement.close();
-            }
-        } catch (SQLException ex) {
-            pl.getLogger().info(ex.getMessage());
-        }
+	protected Statements getStatement(String query) {
+		String trimmedQuery = query.trim();
 
-        return null;
-    }
+		if (trimmedQuery.substring(0, 6).equalsIgnoreCase("SELECT")) {
+			return Statements.SELECT;
+		}
 
-    protected Statements getStatement(String query) {
-        String trimmedQuery = query.trim();
+		if (trimmedQuery.substring(0, 6).equalsIgnoreCase("INSERT")) {
+			return Statements.INSERT;
+		}
 
-        if (trimmedQuery.substring(0, 6).equalsIgnoreCase("SELECT")) {
-            return Statements.SELECT;
-        }
+		if (trimmedQuery.substring(0, 6).equalsIgnoreCase("UPDATE")) {
+			return Statements.UPDATE;
+		}
 
-        if (trimmedQuery.substring(0, 6).equalsIgnoreCase("INSERT")) {
-            return Statements.INSERT;
-        }
+		if (trimmedQuery.substring(0, 6).equalsIgnoreCase("DELETE")) {
+			return Statements.DELETE;
+		}
 
-        if (trimmedQuery.substring(0, 6).equalsIgnoreCase("UPDATE")) {
-            return Statements.UPDATE;
-        }
+		if (trimmedQuery.substring(0, 6).equalsIgnoreCase("CREATE")) {
+			return Statements.CREATE;
+		}
 
-        if (trimmedQuery.substring(0, 6).equalsIgnoreCase("DELETE")) {
-            return Statements.DELETE;
-        }
+		if (trimmedQuery.substring(0, 5).equalsIgnoreCase("ALTER")) {
+			return Statements.ALTER;
+		}
 
-        if (trimmedQuery.substring(0, 6).equalsIgnoreCase("CREATE")) {
-            return Statements.CREATE;
-        }
+		if (trimmedQuery.substring(0, 4).equalsIgnoreCase("DROP")) {
+			return Statements.DROP;
+		}
 
-        if (trimmedQuery.substring(0, 5).equalsIgnoreCase("ALTER")) {
-            return Statements.ALTER;
-        }
+		if (trimmedQuery.substring(0, 8).equalsIgnoreCase("TRUNCATE")) {
+			return Statements.TRUNCATE;
+		}
 
-        if (trimmedQuery.substring(0, 4).equalsIgnoreCase("DROP")) {
-            return Statements.DROP;
-        }
+		if (trimmedQuery.substring(0, 6).equalsIgnoreCase("RENAME")) {
+			return Statements.RENAME;
+		}
 
-        if (trimmedQuery.substring(0, 8).equalsIgnoreCase("TRUNCATE")) {
-            return Statements.TRUNCATE;
-        }
+		if (trimmedQuery.substring(0, 2).equalsIgnoreCase("DO")) {
+			return Statements.DO;
+		}
 
-        if (trimmedQuery.substring(0, 6).equalsIgnoreCase("RENAME")) {
-            return Statements.RENAME;
-        }
+		if (trimmedQuery.substring(0, 7).equalsIgnoreCase("REPLACE")) {
+			return Statements.REPLACE;
+		}
 
-        if (trimmedQuery.substring(0, 2).equalsIgnoreCase("DO")) {
-            return Statements.DO;
-        }
+		if (trimmedQuery.substring(0, 4).equalsIgnoreCase("LOAD")) {
+			return Statements.LOAD;
+		}
 
-        if (trimmedQuery.substring(0, 7).equalsIgnoreCase("REPLACE")) {
-            return Statements.REPLACE;
-        }
+		if (trimmedQuery.substring(0, 7).equalsIgnoreCase("HANDLER")) {
+			return Statements.HANDLER;
+		}
 
-        if (trimmedQuery.substring(0, 4).equalsIgnoreCase("LOAD")) {
-            return Statements.LOAD;
-        }
+		if (trimmedQuery.substring(0, 4).equalsIgnoreCase("CALL")) {
+			return Statements.CALL;
+		}
 
-        if (trimmedQuery.substring(0, 7).equalsIgnoreCase("HANDLER")) {
-            return Statements.HANDLER;
-        }
+		return Statements.SELECT;
+	}
 
-        if (trimmedQuery.substring(0, 4).equalsIgnoreCase("CALL")) {
-            return Statements.CALL;
-        }
+	public class Result {
+		private ResultSet resultSet;
+		private Statement statement;
 
-        return Statements.SELECT;
-    }
+		public Result(Statement statement, ResultSet resultSet) {
+			this.statement = statement;
+			this.resultSet = resultSet;
+		}
 
-    public class Result {
-        private ResultSet resultSet;
-        private Statement statement;
+		public ResultSet getResultSet() {
+			return this.resultSet;
+		}
 
-        public Result(Statement statement, ResultSet resultSet) {
-            this.statement = statement;
-            this.resultSet = resultSet;
-        }
-
-        public ResultSet getResultSet() {
-            return this.resultSet;
-        }
-
-        public void close() {
-            try {
-                this.statement.close();
-                this.resultSet.close();
-            } catch (SQLException e) {}
-        }
-    }
+		public void close() {
+			try {
+				this.statement.close();
+				this.resultSet.close();
+			} catch (SQLException e) {}
+		}
+	}
 }
